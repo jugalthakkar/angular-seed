d0e2e81f94165f17ac5b86030b8bcba6b4f565a0
jugalthakkar <jugal.thakkar@gmail.com>
2017-12-28 11:25:10 +0530

    fixed e2e tests

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,41 +2,41 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
+describe('cabs-ui app', function() {
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('should automatically redirect to / when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/");
   });
 
 
-  describe('view1', function() {
+  describe('user', function() {
 
     beforeEach(function() {
-      browser.get('index.html#!/view1');
+      browser.get('index.html#!/');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    it('view should have one map', function() {
+      expect(element.all(by.css('ng-map')).count()).
+        toBe(1);
     });
 
   });
 
 
-  describe('view2', function() {
+  describe('admin', function() {
 
     beforeEach(function() {
-      browser.get('index.html#!/view2');
+      browser.get('index.html#!/admin');
     });
 
 
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
+      it('view should have one map', function() {
+          expect(element.all(by.css('ng-map')).count()).
+          toBe(1);
+      });
 
   });
 });
